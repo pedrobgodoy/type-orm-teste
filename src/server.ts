958a651075a93b2890688a12cda9552db62e1ebf
PedroBGodoy <pedrohenrique.godoy@outlook.com>
2020-06-23 00:32:21 -0300

    Fix eslint

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,10 @@
-import express from 'express';
+import express from 'express'
 import '@controllers/UsersController'
 
-const app = express();
+const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' })
+  return response.json({ message: 'Hello World' })
 })
 
-app.listen(3333, () => console.log("App listening on port 3333"))
+app.listen(3333, () => console.log('App listening on port 3333'))
