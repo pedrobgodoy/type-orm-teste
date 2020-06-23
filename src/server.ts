a1a6b2e3f86a0e5ac21c5a90fdd5504b193c6ed3
Pedro Godoy <pedrohenrique.godoy@outlook.com>
2020-06-23 00:48:28 -0300

    Add Prettier to Eslint and change Eslint configs

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
-import express from 'express'
-import '@controllers/UsersController'
+import express from 'express';
 
-const app = express()
+const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({ message: 'Hello World' })
-})
+  return response.json({ message: 'Hello World' });
+});
 
-app.listen(3333, () => console.log('App listening on port 3333'))
+app.listen(3333, () => console.log('App listening on port 3333'));
