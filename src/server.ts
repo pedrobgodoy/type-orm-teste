80e9c54914f67f685e260f2f5a374ea300d3936b
Pedro Godoy <pedrohenrique.godoy@outlook.com>
2020-06-23 00:28:21 -0300

    Add Eslint

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import '@controllers/UsersController'
 const app = express();
 
 app.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
+    return response.json({ message: 'Hello World' })
 })
 
 app.listen(3333, () => console.log("App listening on port 3333"))
