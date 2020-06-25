bb778f825eb0e031d24561e6e602bcb63cd38c1f
Pedro Godoy <pedrohenrique.godoy@outlook.com>
2020-06-25 12:58:16 -0300

    Env variable now are added based on NODE_ENV

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import dotenv from 'dotenv';
+
 import app from './app';
 
-const { PORT } = process.env;
+const envFile = process.env.NODE_ENV ? `.env.${process.env.NODE_ENV}` : '.env';
+dotenv.config({ path: envFile });
 
-app.listen(PORT || 3333, () =>
-  console.log(`Server listening on port ${PORT || 3333}`)
+app.listen(process.env.PORT || 3333, () =>
+  console.log(`Server listening on port ${process.env.PORT || 3333}`)
 );
